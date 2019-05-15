61cadcd95df00178bca081628383882f8254e0a2
Kelsey Bajis <bajis.simple.haven@gmail.com>
2019-05-15 18:14:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def long_planeteer_calls(arr)# code an argument here
 end
 
 def find_the_cheese(arr)# code an argument here
-  
+
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   if arr.include?("cheddar")
